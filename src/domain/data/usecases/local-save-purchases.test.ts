ed3997244f52550552f9b95af81d3d495ffe3762
Bianca Ferreira <bianca.ferreira@m4u.com.br>
2020-12-03 15:22:54 -0300

    ensure local save deletes old cache on save

--- src/domain/data/usecases/local-save-purchases.test.ts
+++ src/domain/data/usecases/local-save-purchases.test.ts
@@ -1,16 +1,21 @@
 /* eslint-disable require-jsdoc */
 class LocalSavePurchases {
-  constructor(private readonly cacheStore: CacheStore) {
-
+  constructor(private readonly cacheStore: CacheStore) {}
+  async save(): Promise<void> {
+    this.cacheStore.delete();
   }
 }
 
 interface CacheStore {
-
+  delete(): void;
 }
 
 class CacheStoreSpy implements CacheStore {
   deleteCallsCount = 0;
+
+  delete(): void {
+    this.deleteCallsCount++;
+  }
 }
 
 
@@ -20,5 +25,14 @@ test('Local Save Purchases - not delete cache on init', () => {
   new LocalSavePurchases(cacheStore);
 
   expect(cacheStore.deleteCallsCount).toBe(0);
+});
+
+test('Local Save Purchases - delete old cache on save', async () => {
+  const cacheStore = new CacheStoreSpy();
+
+  const sut = new LocalSavePurchases(cacheStore);
+  await sut.save();
+
+  expect(cacheStore.deleteCallsCount).toBe(1);
 });
 
