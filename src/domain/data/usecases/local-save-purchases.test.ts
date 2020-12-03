b55aef45a9a5f573c8c5a36fcce36e41fc0aa5f7
Bianca Ferreira <bianca.ferreira@m4u.com.br>
2020-12-03 16:16:38 -0300

    refactor

--- src/domain/data/usecases/local-save-purchases.test.ts
+++ src/domain/data/usecases/local-save-purchases.test.ts
@@ -7,7 +7,7 @@ class LocalSavePurchases {
 }
 
 interface CacheStore {
-  delete(): void;
+  delete:() => void;
 }
 
 class CacheStoreSpy implements CacheStore {
@@ -18,21 +18,29 @@ class CacheStoreSpy implements CacheStore {
   }
 }
 
+type SutTypes = {
+  sut: LocalSavePurchases,
+  cacheStore: CacheStoreSpy
+}
+
+const makeSut = (): SutTypes => {
+  const cacheStore = new CacheStoreSpy();
+  const sut = new LocalSavePurchases(cacheStore);
+
+  return {
+    sut,
+    cacheStore};
+};
+
 
 test('Local Save Purchases - not delete cache on init', () => {
-  const cacheStore = new CacheStoreSpy();
-
-  new LocalSavePurchases(cacheStore);
-
+  const {cacheStore} = makeSut();
   expect(cacheStore.deleteCallsCount).toBe(0);
 });
 
 test('Local Save Purchases - delete old cache on save', async () => {
-  const cacheStore = new CacheStoreSpy();
-
-  const sut = new LocalSavePurchases(cacheStore);
+  const {sut, cacheStore} = makeSut();
   await sut.save();
-
   expect(cacheStore.deleteCallsCount).toBe(1);
 });
 
